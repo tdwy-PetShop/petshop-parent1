4d78f2f835ef59d6eb6b466f9e16e73ca4a47ded
nao0403 <1826267627@qq.com>
2020-09-24 14:40:02 +0800

    删除后台多余html

--- petshop-parent2/petshop-index/src/main/java/com/tdwy/petshopindex/BackAction.java
+++ petshop-parent2/petshop-index/src/main/java/com/tdwy/petshopindex/BackAction.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BackAction {
 	
-	@GetMapping("/back/")
+	@GetMapping("/")
 	public  String index() {
 		return "index";
 	}
